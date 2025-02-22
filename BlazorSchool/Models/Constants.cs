adae26eb7094dfff30827c2d50f24d083a67eae0
Ryan Jiang <ryan94j@gmail.com>
2025-02-21 16:31:53 -0800

    completed functionality and then changed base url to /

--- BlazorSchool/Models/Constants.cs
+++ BlazorSchool/Models/Constants.cs
@@ -4,6 +4,6 @@ namespace BlazorSchool.Models;
 
 public class Constants
 {
-    public const string BASE_URL = "https://httpwebapilp.azurewebsites.net/";
+    public const string BASE_URL = "/";
 }
 
